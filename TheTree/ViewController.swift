81ff1e0d183ba2b55b23729da8b6708649254359
Ethan <s017287@students.lmsd.org>
2020-01-15 09:14:20 -0500

    lose apple

--- TheTree/ViewController.swift
+++ TheTree/ViewController.swift
@@ -10,9 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var img: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        var lives = 7
+        
+        
+        
+        
+        func gainLetter(){
+            
+        }
+        
+        func loseApple(){
+            img.image = UIImage(named: "Tree \(lives)")
+        lives -= 1
+            //print("")
+        }
+        
+        
+        
+        
+        
+
+    
     }
 
 
