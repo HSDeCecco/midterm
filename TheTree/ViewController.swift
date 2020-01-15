d2e9d3cffb1f8ddb414a8aabb7d5eb9dd8cc4e13
Rob <rpszhzs@gmail.com>
2020-01-15 09:28:14 -0500

    Storyboard Version 1

--- TheTree/ViewController.swift
+++ TheTree/ViewController.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 	
+
+    @IBOutlet weak var wordLabel1: UILabel!
+    @IBOutlet weak var wordLabel2: UILabel!
+    @IBOutlet weak var wordLabel3: UILabel!
+    @IBOutlet weak var wordLabel4: UILabel!
+    @IBOutlet weak var wordLabel5: UILabel!
+    @IBOutlet weak var wordLabel6: UILabel!
+    @IBOutlet weak var wordLabel7: UILabel!
+    @IBOutlet weak var wordLabel8: UILabel!
+    @IBOutlet weak var wordLabel9: UILabel!
+    @IBOutlet weak var wordLabel10: UILabel!
+    @IBOutlet weak var wordLabel11: UILabel!
+    @IBOutlet weak var wordLabel12: UILabel!
+    @IBOutlet weak var wordLabel13: UILabel!
+    @IBOutlet weak var wordLabel14: UILabel!
+    @IBOutlet weak var wordLabel15: UILabel!
+    @IBOutlet weak var wordLabel16: UILabel!
+    @IBOutlet weak var wordLabel17: UILabel!
+    @IBOutlet weak var wordLabel18: UILabel!
+    @IBOutlet weak var wordLabel19: UILabel!
+    @IBOutlet weak var wordLabel20: UILabel!
+    @IBOutlet weak var wordLabel21: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
